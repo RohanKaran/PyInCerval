77392ca2fd9ad51fa591a1af3f6d7619e0e9365e
RohanKaran <rohankaran001@gmail.com>
2025-03-05 00:33:52 +0530

    Add interval division and subtraction functions

--- pyincerval/c_interval.c
+++ pyincerval/c_interval.c
@@ -15,3 +15,23 @@ Interval interval_mul(Interval a, Interval b) {
     result.upper = fmax(fmax(p1, p2), fmax(p3, p4));
     return result;
 }
+
+Interval interval_div(Interval a, Interval b) {
+	if (b.lower <= 0 && b.upper >= 0) {
+		// Division by an interval that contains zero is undefined
+		return (Interval){NAN, NAN};
+	}
+	Interval result;
+	double p1 = a.lower / b.lower;
+	double p2 = a.lower / b.upper;
+	double p3 = a.upper / b.lower;
+	double p4 = a.upper / b.upper;
+	result.lower = fmin(fmin(p1, p2), fmin(p3, p4));
+	result.upper = fmax(fmax(p1, p2), fmax(p3, p4));
+	return result;
+}
+
+Interval interval_sub(Interval a, Interval b) {
+	Interval result = {a.lower - b.upper, a.upper - b.lower};
+	return result;
+}
